62622aee6876f1901e1ce41d1d9620551c70377b
vishal <vishal.saini.99154@gmail.com>
2024-07-26 16:39:55 +0530

    added

---
+++ components/Spinner.jsx
@@ -0,0 +1,35 @@
+'use client';
+const Spiner = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spiner;
